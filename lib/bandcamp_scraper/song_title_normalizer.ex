fcc473186a840a0c22cfb5557bfedf158d7131bb
Dave Frey <perfectritone@gmail.com>
2023-11-05 14:43:47 -0500

    Adding song title normalizers

--- lib/bandcamp_scraper/song_title_normalizer.ex
+++ lib/bandcamp_scraper/song_title_normalizer.ex
@@ -18,6 +18,16 @@ defmodule BandcampScraper.SongTitleNormalizer do
     Repo.all(q)
   end
 
+  def all_set_songs do
+    SetSong
+    |> Repo.all
+  end
+
+  def force_normalize_song_titles do
+    all_set_songs()
+    |> Enum.map(&associate_song/1)
+  end
+
   def associate_song(set_song) do
     normalized_title = normalize_song_title(set_song.title)
 
@@ -42,8 +52,22 @@ defmodule BandcampScraper.SongTitleNormalizer do
 
   def normalize_song_title(raw_title) do
     raw_title
-    |> String.downcase
+    |> downcase
+    |> remove_punctuation
+    |> remove_accents
+    |> remove_trailing_dates
+    |> remove_xl
+    |> remove_acoustic
+    |> trim_whitespace
   end
+
+  def downcase(string), do: String.downcase(string)
+  def remove_punctuation(string), do: String.replace(string, ~r/\.?'?>?\*?\^?/, "")
+  def remove_accents(string), do: String.replace(string, ~r/é/, "e")
+  def remove_trailing_dates(string), do: String.replace(string, ~r/\(\d+\)\Z/, "")
+  def remove_xl(string), do: String.replace(string, ~r/\(?xl\)?\Z/, "")
+  def remove_acoustic(string), do: String.replace(string, ~r/\(?acoustic\)?\Z/, "")
+  def trim_whitespace(string), do: String.trim(string)
 
   def delete_all_songs do
     Repo.delete_all(Song)
