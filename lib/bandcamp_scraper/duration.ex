a237393053d76ebfe79f7f8fb03fe9b5aaf7286c
Dave Frey <perfectritone@gmail.com>
2023-12-14 00:04:43 -0500

    Displaying duration on set for songs and padding seconds

--- lib/bandcamp_scraper/duration.ex
+++ lib/bandcamp_scraper/duration.ex
@@ -1,7 +1,10 @@
 defmodule BandcampScraper.Duration do
   def seconds_to_human_readable(seconds) do
     minutes = Integer.floor_div(seconds, 60)
-    remaining_seconds = seconds - minutes * 60
+    remaining_seconds =
+      seconds - minutes * 60
+      |> Integer.to_string
+      |> String.pad_leading(2, "0")
 
     "#{minutes}:#{remaining_seconds}"
   end
