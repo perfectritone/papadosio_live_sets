df72d55039f8353c6e5ee6d0c01776c9eaacbf7c
Dave Frey <perfectritone@gmail.com>
2025-11-30 02:42:39 -0500

    scrape featured albums as well. i was missing the newest

--- lib/bandcamp_scraper/scraper.ex
+++ lib/bandcamp_scraper/scraper.ex
@@ -21,8 +21,57 @@ defmodule BandcampScraper.Scraper do
   end
 
   def extract_sets(html) do
-    extract_data_client_items(html)
-    |> transform_client_items
+    # Extract from both data-client-items JSON and direct HTML links
+    # Some featured/recent albums are only in the HTML, not in data-client-items
+    json_sets = extract_data_client_items(html) |> transform_client_items
+    html_sets = extract_html_album_links(html)
+
+    # Merge, preferring json_sets for duplicates (they have more data)
+    json_urns = MapSet.new(Enum.map(json_sets, & &1.urn))
+
+    new_html_sets = Enum.reject(html_sets, fn set -> MapSet.member?(json_urns, set.urn) end)
+
+    new_html_sets ++ json_sets
+  end
+
+  def extract_html_album_links(html) do
+    {:ok, document} = Floki.parse_document(html)
+
+    # Find album links in the music grid that have images (to get thumbnails)
+    Floki.find(document, "#music-grid li a[href^=\"/album/\"]")
+    |> Enum.map(fn link_element ->
+      href = Floki.attribute(link_element, "href") |> List.first()
+      img = Floki.find(link_element, "img") |> List.first()
+      img_src = if img, do: Floki.attribute(img, "src") |> List.first(), else: nil
+
+      # Try to get title from various places
+      title = extract_album_title(link_element, href)
+
+      %{
+        title: title |> DateExtractor.strip_zero_width(),
+        urn: href,
+        thumbnail: img_src
+      }
+    end)
+    |> Enum.filter(fn set -> set.urn != nil end)
+    |> Enum.uniq_by(& &1.urn)
+  end
+
+  defp extract_album_title(link_element, href) do
+    # Try to find title in link text or nearby elements
+    title_text = Floki.find(link_element, ".title") |> Floki.text() |> String.trim()
+
+    if title_text != "" do
+      title_text
+    else
+      # Fall back to extracting from URL
+      href
+      |> String.replace("/album/", "")
+      |> String.replace("-", " ")
+      |> String.split()
+      |> Enum.map(&String.capitalize/1)
+      |> Enum.join(" ")
+    end
   end
 
   def extract_data_client_items(html) do
