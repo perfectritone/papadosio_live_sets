219fb0aa72743ef9a64c9f7626dabef4f44a3908
Dave Frey <perfectritone@gmail.com>
2025-11-30 00:51:21 -0500

    seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,18 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias BandcampScraper.Accounts
+
+# Create default admin user if it doesn't exist
+unless Accounts.get_user_by_username("merp") do
+  Accounts.create_user(%{
+    username: "merp",
+    password: "dosioposio",
+    role: "admin"
+  })
+  |> case do
+    {:ok, _user} -> IO.puts("Created admin user 'merp'")
+    {:error, changeset} -> IO.puts("Failed to create admin user: #{inspect(changeset.errors)}")
+  end
+end
