92cd052c0443486218e7023a4023f372ffd6dca0
Dave Frey <perfectritone@gmail.com>
2025-11-30 00:02:34 -0500

    clickable variants

--- lib/bandcamp_scraper_web/controllers/song_html.ex
+++ lib/bandcamp_scraper_web/controllers/song_html.ex
@@ -1,6 +1,8 @@
 defmodule BandcampScraperWeb.SongHTML do
   use BandcampScraperWeb, :html
 
+  alias BandcampScraper.Music.Set
+
   embed_templates "song_html/*"
 
   @doc """
@@ -10,4 +12,9 @@ defmodule BandcampScraperWeb.SongHTML do
   attr :action, :string, required: true
 
   def song_form(assigns)
+
+  @doc """
+  Returns the effective date for a set.
+  """
+  def effective_date(set), do: Set.effective_date(set)
 end
